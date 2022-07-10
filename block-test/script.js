94d41ff33a8ac9c2f73f433f014aa75a9c543828
Refusado <orefusado@gmail.com>
2022-07-10 01:06:56 -0300

    sistema captação e verificação de peça selecionada

--- block-test/script.js
+++ block-test/script.js
@@ -1,20 +1,38 @@
 const bloco = document.getElementById("block");
-const pieceText = document.getElementById('selected-piece');
+const corrPieceText = document.getElementById('correct-piece');
+const currPieceText = document.getElementById('current-piece');
 
-pieceText.style.backgroundColor = '#00000015';
+const imageHeight = 281;
+const correctPiece = 2;
+corrPieceText.innerText = correctPiece;
 
-let currentPiece = 0;
+let currentPosition = bloco.scrollTop / imageHeight;
+let timer = null;
 
-bloco.addEventListener('scroll', (event) => {
-    currentPiece = Math.round(bloco.scrollTop / 281);
-    pieceText.innerText = currentPiece;
+bloco.addEventListener('scroll', () => {
+    bloco.style.outlineColor = "";
 
-    if (bloco.scrollTop / 281 > 7) {
-        bloco.scrollTop = 281 * 7;
+    let currentPiece = Math.round(bloco.scrollTop / imageHeight);
+
+    bloco.addEventListener('touchend', () => {
+        setTimeout(() => {
+            bloco.scrollTop = Math.round(bloco.scrollTop);
+        }, 200);
+    });
+
+    if (timer !== null) {
+        clearTimeout(timer);
     }
-
+    timer = setTimeout(() => {
+        currPieceText.innerText = currentPiece;
+        verifyPiece(currentPiece, correctPiece);
+    }, 2000);
 })
 
-if (currentPiece = 4) {
-    console.log("Bloco 1 completo");
+
+
+function verifyPiece(curr, corr) {
+    if (curr === corr) {
+        bloco.style.outlineColor = "green";
+    }
 }
