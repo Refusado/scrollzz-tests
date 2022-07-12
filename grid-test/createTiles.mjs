63d09c9c0093036c2ab34bb89cd8463f02648556
Refusado <orefusado@gmail.com>
2022-07-12 01:12:16 -0300

    sistema de embaralhamento

--- grid-test/createTiles.mjs
+++ grid-test/createTiles.mjs
@@ -3,23 +3,51 @@ export const createTiles = (tilesNo, columns, piecesNo, type) => {
 
     let newPiece, newTile;
 
-    for (let i = 0; i < tilesNo; i++) {
+
+    
+    let imageSrc = [];
+    for (let i = 0; i < piecesNo; i++) {
+        imageSrc.push("../img/" + type + `/piece-${i + 1}.png`);
+    }
+
+    for (let i = 1; i <= tilesNo; i++) {
+        shuffle(imageSrc);
+
         newTile = document.createElement('div');
         newTile.setAttribute('class', 'tile');
 
-        for (let i = 0; i < piecesNo; i++) {
-            let imageSrc = "../img/" + type + `/piece-${i + 1}.png`;
-            console.log(imageSrc);
+        
+        if (imageSrc[0].match(/\d/)[0] == i) {
+            i--;
+            continue;
+        }
+
+        for (let ii = 0; ii < piecesNo; ii++) {
 
             newPiece = document.createElement('img');
-            newPiece.setAttribute('src', imageSrc);
+            newPiece.setAttribute('src', imageSrc[ii]);
 
             newTile.appendChild(newPiece);
         }
+
+        // console.log(i + 1);
 
         container.appendChild(newTile);
     }
 
     container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     container.style.gridTemplateRows    = `repeat(${tilesNo / columns}, ${1 / piecesNo}fr)`;
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const x = Math.floor(Math.random() * (i + 1));
+
+        [arr[i], arr[x]] = [arr[x], arr[i]];
+    }
+
+    return arr;
 }
